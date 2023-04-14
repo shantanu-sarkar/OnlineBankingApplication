0ee481abd61ae02d6500d573a0552b880b9e1253
Shantanu Sarkar <shantanuplussarkar@gmail.com>
2023-04-14 12:06:40 +0530

    Initialize Online Banking Application Project
    Dependencies added: MySQL Driver, Spring Data JPA, Spring Security, Spring Web

---
+++ src/test/java/com/kpmg/OnlineBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
