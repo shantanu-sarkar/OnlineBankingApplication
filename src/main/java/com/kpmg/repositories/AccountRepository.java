165e526bbd5fa9ba1354a09ecf513c9ebe430faa
Ankit Rajak <rajakankit66@gmail.com>
2023-04-17 14:41:56 +0530

    Add Account and User Repositories

---
+++ src/main/java/com/kpmg/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.kpmg.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kpmg.entities.Account;
+
+public interface AccountRepository extends JpaRepository<Account,Integer> {
+
+}
--- src/main/java/com/kpmg/repositories/UserRepository.java
+++ src/main/java/com/kpmg/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.kpmg.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kpmg.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
 
 }
