24cadedfc43c835249c15e1df61124cc199e1837
saurabhraj23 <saurabhraj239@gmail.com>
2023-04-27 09:27:31 +0530

    uthenttication Implemented

--- src/main/java/com/kpmg/OnlineBankingApplication.java
+++ src/main/java/com/kpmg/OnlineBankingApplication.java
@@ -3,6 +3,7 @@ package com.kpmg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
